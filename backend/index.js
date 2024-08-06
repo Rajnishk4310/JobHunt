e3803b9f438c1e76d4410cb706157ce12c341a6c
Rajnish <rajnisharyankumar@gmail.com>
2024-08-06 19:20:43 +0530

    resolve CORS issue

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: ["http://localhost:5173", "https://job-hunt-rbtj.vercel.app"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
 
 // api routes
@@ -32,6 +36,8 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+app.options('*', cors(corsOptions)); // Preflight request handling
 
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}`);
